af33e909331c62564f7e1b14d10cdc9d7ca72353
Taha Yavuz <45938952+tahayvz@users.noreply.github.com>
2021-02-23 22:05:55 +0300

    adding bcrypt15 encoder

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("{sha256}1c41d9a53f84d198b4406a1f81136f303adcd5a87dcfa1f3efb8263d95d46bef5f5b0cbbb6b0ff90")
                 .roles("USER");
 
-        auth.inMemoryAuthentication().withUser("scott").password("{ldap}{SSHA}emd6D+MJW8sVuNpxVaYtUYiC6BbkxUXxUe5AzA==").roles("CUSTOMER");
+        auth.inMemoryAuthentication().withUser("scott").password("{bcrypt15}$2a$15$baOmQtw8UqWZRDQhMFPFj.xhkkWveCTQHe4OBdr8yw8QshejiSbI6").roles("CUSTOMER");
     }
 
     //    @Override
--- src/main/java/guru/sfg/brewery/security/SfgPasswordEncoderFactories.java
+++ src/main/java/guru/sfg/brewery/security/SfgPasswordEncoderFactories.java
@@ -7,15 +7,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by jt on 6/17/20.
- */
 public class SfgPasswordEncoderFactories {
 
     public static PasswordEncoder createDelegatingPasswordEncoder() {
-        String encodingId = "bcrypt";
+        String encodingId = "bcrypt15";
         Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put(encodingId, new BCryptPasswordEncoder());
+        encoders.put(encodingId, new BCryptPasswordEncoder(15));
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
         encoders.put("ldap", new org.springframework.security.crypto.password.LdapShaPasswordEncoder());
         encoders.put("noop", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance());
         encoders.put("sha256", new org.springframework.security.crypto.password.StandardPasswordEncoder());
