1253f2d4ceeaa87f461f4158acacf491821aa812
Taha Yavuz <45938952+tahayvz@users.noreply.github.com>
2021-03-07 14:56:14 +0300

    Adding filter for role

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,45 +1,20 @@
 package guru.sfg.brewery.config;
 
-//import guru.sfg.brewery.security.RestHeaderAuthFilter;
-//import guru.sfg.brewery.security.RestUrlAuthFilter;
 import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager){
-//        RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
-//        filter.setAuthenticationManager(authenticationManager);
-//        return filter;
-//    }
-//
-//    public RestUrlAuthFilter restUrlAuthFilter(AuthenticationManager authenticationManager){
-//        RestUrlAuthFilter filter = new RestUrlAuthFilter(new AntPathRequestMatcher("/api/**"));
-//        filter.setAuthenticationManager(authenticationManager);
-//        return filter;
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
-////                UsernamePasswordAuthenticationFilter.class);
-//                UsernamePasswordAuthenticationFilter.class)
-//                .csrf().disable();
-//
-//        http.addFilterBefore(restUrlAuthFilter(authenticationManager()),
-//                UsernamePasswordAuthenticationFilter.class);
 
         http
                 .authorizeRequests(authorize -> {
@@ -48,6 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
                             .antMatchers("/beers/find", "/beers*").permitAll()
                             .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                            .mvcMatchers(HttpMethod.DELETE, "/api/v1/beer/**").hasRole("ADMIN")
                             .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
                 } )
                 .authorizeRequests()
@@ -57,8 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and().csrf().disable();
 
-                //h2 console config
-                http.headers().frameOptions().sameOrigin();
+        //h2 console config
+        http.headers().frameOptions().sameOrigin();
     }
 
     @Bean
@@ -66,20 +42,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    // @Override
+    //   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    // auth.userDetailsService(this.jpaUserDetailsService).passwordEncoder(passwordEncoder());
+
 //        auth.inMemoryAuthentication()
 //                .withUser("spring")
-//                .password("{bcrypt}$2a$10$b.LK6FN2xk1FGNyQaPUMX.HYiQADhHY5ufhPluXuI5mKWBmhW3vUa")
+//                .password("{bcrypt}$2a$10$7tYAvVL2/KwcQTcQywHIleKueg4ZK7y7d44hKyngjTwHCDlesxdla")
 //                .roles("ADMIN")
 //                .and()
 //                .withUser("user")
-//                .password("{sha256}1c41d9a53f84d198b4406a1f81136f303adcd5a87dcfa1f3efb8263d95d46bef5f5b0cbbb6b0ff90")
+//                .password("{sha256}1296cefceb47413d3fb91ac7586a4625c33937b4d3109f5a4dd96c79c46193a029db713b96006ded")
 //                .roles("USER");
-
+//
 //        auth.inMemoryAuthentication().withUser("scott").password("{bcrypt15}$2a$15$baOmQtw8UqWZRDQhMFPFj.xhkkWveCTQHe4OBdr8yw8QshejiSbI6").roles("CUSTOMER");
-//        auth.inMemoryAuthentication().withUser("scott").password("{bcrypt10}$2a$10$tvgJyk6jLMwJ4cQa1zzkt.LWeYvcNvilF9Ej5XbIeWrCG8TzK49MS").roles("CUSTOMER");
-//    }
+    //  }
 
     //    @Override
 //    @Bean
@@ -98,6 +75,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //        return new InMemoryUserDetailsManager(admin, user);
 //    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
